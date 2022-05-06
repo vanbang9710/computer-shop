f8e66591205b0727ecf25a4351fc4ea576c5d994
hieuvuhv34 <hieuvuhv34@gmail.com>
2022-05-07 00:49:32 +0700

    Add some features and test rwd

--- src/App.js
+++ src/App.js
@@ -2,18 +2,29 @@ import Navbar from "./components/Navbar";
 import Sidebar from "./components/Sidebar";
 import Feed from "./components/Feed";
 import Rightbar from "./components/Rightbar";
-import { Box, Stack } from "@mui/material";
+import { Box, createTheme, Stack, ThemeProvider } from "@mui/material";
+import Add from "./components/Add";
+import { useState } from "react";
 
 function App() {
+  const [mode, setMode] = useState("light");
+  const mainTheme = createTheme({
+    palette: {
+      mode: mode
+    }
+  });
   return (
-    <Box>
-      <Navbar />
-      <Stack direction="row" spacing={0} justifyContent="space-between">
-        <Sidebar />
-        <Feed />
-        <Rightbar />
-      </Stack>
-    </Box>
+    <ThemeProvider theme={mainTheme}>
+      <Box bgcolor={"background.default"} color={"text.primary"}>
+        <Navbar />
+        <Stack direction="row" spacing={0} justifyContent="space-between">
+          <Sidebar setMode={setMode} mode={mode} />
+          <Feed />
+          <Rightbar />
+        </Stack>
+        <Add />
+      </Box>
+    </ThemeProvider>
   );
 }
 
